cd5b22d214d36be46adc60784b4e180103e3e7b5
AG <415204643@qq.com>
2021-01-26 17:06:55 +0800

    添加.gitignore和Python3的示例模板

---
+++ Python3答题的模板.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3 
+# -*- coding: utf-8 -*-
+# @lc app=leetcode.cn id=0 lang=python3
+#
+# [0] Python3的模板
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def demo(self, args1: List[int]):
+        return None
+# @lc code=end
+if __name__ == "__main__":
+    nums = ['t', 'e', 's', 't']
+    assert (Solution().demo(nums) == None)
